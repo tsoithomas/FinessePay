ffaa1965214b99b008e54b4676106056b98be7ab
Thomas Tsoi <tommasinino@yahoo.com>
2022-04-14 12:55:05 +0000

    update

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 SECRET_KEY = "iwogieog2598fhifwhgn"
-GITHUB_CLIENT_ID = 'b5624ec9aabafd30d2ca'
-GITHUB_CLIENT_SECRET = '5cf774df5acbcd5446219addbd5114d59559ac35'
-MYSQL_USER = "root"
-MYSQL_PASS = ""
-MYSQL_HOST = "localhost"
-MYSQL_DATABASE = "finesse"
+GITHUB_CLIENT_ID = 'e328a14be11637caf2a3'
+GITHUB_CLIENT_SECRET = 'f74f4e006c9abb67d39c3bd70520045edfc1d796'
+MYSQL_USER = "finesse"
+MYSQL_PASS = "hkbuCOMP7300"
+MYSQL_HOST = "finesse.mysql.pythonanywhere-services.com"
+MYSQL_DATABASE = "finesse$default"
