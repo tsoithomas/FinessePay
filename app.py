e8040fab8a7ed67ac663da82aca997ba08b9b112
tommasinino <97960464+tommasinino@users.noreply.github.com>
2022-04-16 21:00:16 +0800

    update

--- app.py
+++ app.py
@@ -80,7 +80,49 @@ def pay():
     for (category_id, category_name) in rows:
         categories.append({"id": category_id, "name": category_name})
         
+    cursor.close()
+    cnx.close()
+        
     return render_template('pay.html', title=' - Send payment', login=github_user['login'], categories=categories)
+ 
+
+@app.route("/pay_submit", methods=['POST'])
+def pay_submit():
+    if not github.authorized:
+        return redirect('/login')
+        
+    github_user = github.get("/user").json()
+    
+    payer = get_user(github_user['login'])
+    print(payer, file=sys.stderr)
+    payer_id = payer["user_id"]
+    
+    payee = get_user(request.form.get('payee'))
+    payee_id = payee["user_id"]
+    
+    amount = request.form.get('amount')
+    category_id = request.form.get('category')
+
+    if payee_id is not None:
+        cnx = mysql.connector.connect(user=config.MYSQL_USER, password=config.MYSQL_PASS, host=config.MYSQL_HOST, database=config.MYSQL_DATABASE)
+        cursor = cnx.cursor(buffered=True)
+        
+        insert_payment = ("INSERT INTO payment (payer_id, payee_id, amount, category_id, payment_time) VALUES(%s, %s, %s, %s, NOW())")
+        cursor.execute(insert_payment, (payer_id, payee_id, amount, category_id))
+        
+        update_payer_balance = ("UPDATE account SET balance = balance - %s WHERE user_id = %s")
+        cursor.execute(update_payer_balance, (amount, payer_id))
+        
+        update_payee_balance = ("UPDATE account SET balance = balance + %s WHERE user_id = %s")
+        cursor.execute(update_payee_balance, (amount, payee_id))
+        
+        cnx.commit()
+        
+
+    cursor.close()
+    cnx.close()
+        
+    return payee
  
 
 
@@ -90,11 +132,15 @@ def schedule():
         return redirect('/login')
         
     github_user = github.get("/user").json()
-    
 
     cnx = mysql.connector.connect(user=config.MYSQL_USER, password=config.MYSQL_PASS, host=config.MYSQL_HOST, database=config.MYSQL_DATABASE)
     cursor = cnx.cursor(buffered=True)
      
+    query = ("SELECT user_id, balance FROM account WHERE username = %s")
+    results = cursor.execute(query, (github_user["login"], ))
+    if cursor.rowcount > 0:
+        (user_id, balance) = cursor.fetchone()
+
     query = ("SELECT user_id, balance FROM account WHERE username = %s")
     cursor.execute(query, (github_user["login"], ))
     if cursor.rowcount > 0:
@@ -107,6 +153,9 @@ def schedule():
     for (category_id, category_name) in rows:
         categories.append({"id": category_id, "name": category_name})
         
+    cursor.close()
+    cnx.close()
+        
     return render_template('schedule.html', title=' - Schedule payment', login=github_user['login'], categories=categories)
  
  
@@ -117,8 +166,74 @@ def history():
         
     github_user = github.get("/user").json()
 
+    user = get_user(github_user['login'])
+    user_id = user["user_id"]
+
+    cnx = mysql.connector.connect(user=config.MYSQL_USER, password=config.MYSQL_PASS, host=config.MYSQL_HOST, database=config.MYSQL_DATABASE)
+    cursor = cnx.cursor(buffered=True)
+     
+    records = []
+    current_date = ""
+    date_records = []
+    query = ("""SELECT 
+                    payment_id, payer.user_id, payer.username, payer.nickname, 
+                    payee.user_id, payee.username, payee.nickname, 
+                    amount, category.category_name, DATE_FORMAT(payment_time, "%e %b %Y") 
+                FROM payment 
+                LEFT JOIN account AS payer ON payment.payer_id = payer.user_id
+                LEFT JOIN account AS payee ON payment.payee_id = payee.user_id
+                LEFT JOIN category ON payment.category_id = category.category_id
+                WHERE payment.payer_id = %s OR payment.payee_id = %s
+                ORDER BY payment_time DESC""")
+    cursor.execute(query, (user_id, user_id))
+    rows = cursor.fetchall()
+    i = 0
+    rowcount = cursor.rowcount
+    
+    for (payment_id, payer_id, payer_username, payer_nickname, payee_id, payee_username, payee_nickname, amount, category, date) in rows:
+        if i == 0:
+            current_date = date
+
+        if date != current_date:
+            records.append({"date": current_date, "transactions": date_records})
+            current_date = date
+            date_records = []
+
+        if payer_nickname == "":
+            payer = payer_username
+        else:
+            payer = payer_nickname
+            
+        if payee_nickname == "":
+            payee = payee_username
+        else:
+            payee = payee_nickname
+    
+        if user_id == payer_id:
+            party = payee
+            amount = -amount
+            style = "red"
+        elif user_id == payee_id:
+            party = payer
+            style = "green"
+        else:
+            party = "Error"
+            style = "error"
+
+        date_records.append({"party": party, "amount": amount, "category": category, "style": style})
         
-    return render_template('history.html', title=' - Payment history', login=github_user['login'])
+        i += 1
+        
+        if i == rowcount:
+            records.append({"date": current_date, "transactions": date_records})
+
+    
+    cursor.close()
+    cnx.close()
+
+    #records = [{"date": "2022-05-03", "transactions": [{"party": "Thomas Tsoi", "amount": 234.20}]}]
+        
+    return render_template('history.html', title=' - Payment history', login=github_user['login'], balance=user["balance"], records=records)
  
  
  
@@ -129,6 +244,8 @@ def budget():
         
     github_user = github.get("/user").json()
 
+
+    
         
     return render_template('budget.html', title=' - Budget', login=github_user['login'])
  
@@ -165,9 +282,23 @@ def search():
     cnx.close()
     
     return json.dumps(result) 
- 
- 
-  
+
+def get_user(username: str):
+    cnx = mysql.connector.connect(user=config.MYSQL_USER, password=config.MYSQL_PASS, host=config.MYSQL_HOST, database=config.MYSQL_DATABASE)
+    cursor = cnx.cursor(buffered=True)
+
+    query = ("SELECT user_id, nickname, balance, budget FROM account WHERE username = %s")
+    results = cursor.execute(query, (username, ))
+    if cursor.rowcount > 0:
+        (user_id, nickname, balance, budget) = cursor.fetchone()
+        user = {"user_id": user_id, "nickname": nickname, "balance": balance, "budget": budget}
+    else:
+        user = {"user_id": None, "nickname": None, "balance": None, "budget": None}
+
+    cursor.close()
+    cnx.close()
+    return user
+
 # main driver function
 if __name__ == '__main__':
     app.run(ssl_context=('cert.pem', 'key.pem'))
